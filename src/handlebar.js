4a42f140548ae4150d87ba81eb2adc6debecf3eb
getify <getify@gmail.com>
2010-02-16 12:19:27 -0600

    lame bug in V8 v2.0.6.4 that "instanceof" doesn't work.

--- src/handlebar.js
+++ src/handlebar.js
@@ -269,7 +269,7 @@
 										out[cnt++] = "out[c++]=fnStore[tmp].func(_);";
 										out[cnt++] = "}";
 										out[cnt++] = "catch(terr){";	// if so, throw error
-										out[cnt++] = "if(terr instanceof $HB.TemplateError){";	// already a typed TemplateError
+										out[cnt++] = "if(terr.constructor === $HB.TemplateError){";	// already a typed TemplateError
 										out[cnt++] = "throw terr;";	// so, just re-throw
 										out[cnt++] = "}";
 										out[cnt++] = "else{";	// general exception, so cast as TemplateError
@@ -461,12 +461,12 @@
 					return cb(out);
 				}
 				catch (err) {
-					if (!(err instanceof publicAPI.TemplateError)) {	// some generic JS interpreter exception
+					if (!(err.constructor === publicAPI.TemplateError)) {	// some generic JS interpreter exception
 						err = new publicAPI.TemplateError(err.message);	// cast generic exception as Template Error
 						err.TemplateName(file+id);
 					}
 					
-					if (err instanceof publicAPI.MissingTemplateError) {	// was a missing template, try once to request it to resolve the dependency error
+					if (err.constructor === publicAPI.MissingTemplateError) {	// was a missing template, try once to request it to resolve the dependency error
 						var requested_template = templateURLsplit(err.Value());
 						var current_template = templateURLsplit(err.TemplateName());
 						if (!requested_template.src) requested_template.src = current_template.src;
